c4633cb78402ec3b2104bbf95d8c64195f874b78
ferdeolazabal <ferdeolazabal@gmail.com>
2024-04-12 23:36:53 -0300

    Delete console.log

--- src/data-source.ts
+++ src/data-source.ts
@@ -22,7 +22,6 @@ if (process.env.NODE_ENV === "development") {
   });
 }
 if (process.env.NODE_ENV === "production") {
-  console.log("DATABASE_URL: ", `${process.env.DATABASE_URL}`);
   if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL must be set");
   }
